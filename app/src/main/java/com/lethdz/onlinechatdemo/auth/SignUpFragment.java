c7e6b4196dc61f09a5897ea50039d63c8be38ba6
dongnp2 <iamdimio@gmail.com>
2020-04-01 11:16:59 +0700

    Fix signup process

--- app/src/main/java/com/lethdz/onlinechatdemo/auth/SignUpFragment.java
+++ app/src/main/java/com/lethdz/onlinechatdemo/auth/SignUpFragment.java
@@ -22,6 +22,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.lethdz.onlinechatdemo.FirebaseSingleton;
 import com.lethdz.onlinechatdemo.HomeActivity;
+import com.lethdz.onlinechatdemo.ProfileActivity;
 import com.lethdz.onlinechatdemo.R;
 import com.lethdz.onlinechatdemo.modal.User;
 
@@ -172,7 +173,7 @@ public class SignUpFragment extends Fragment {
 
     private void updateUI(User currentUser) {
         if(currentUser != null) {
-            Intent intent = new Intent(getContext(), HomeActivity.class);
+            Intent intent = new Intent(getContext(), ProfileActivity.class);
             this.startActivity(intent);
             getActivity().finish();
         }
